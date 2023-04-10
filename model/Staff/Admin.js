79e76970465e12e19e84669edabe585a000d3c05
yasith <wimukthiyasith@gmail.com>
2023-04-10 16:44:34 +0530

    implement admin functions

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -19,26 +19,41 @@ const adminSchema = new mongoose.Schema(
       type: String,
       default: "admin",
     },
+    academicTerms: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "AcademicTerm",
+      },
+    ],
+    academicYears: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "AcademicYear",
+      },
+    ],
+    classLevels: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ClassLevel",
+      },
+    ],
+    teachers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Teacher",
+      },
+    ],
+    students: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
-//Hash password
-adminSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-  //salt
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-//verifyPassword
-adminSchema.methods.verifyPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 //model
 const Admin = mongoose.model("Admin", adminSchema);
