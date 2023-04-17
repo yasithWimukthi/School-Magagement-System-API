8942cf8c26734f5cd1542af1fdeda9a55c5b2c6f
yasith <wimukthiyasith@gmail.com>
2023-04-17 10:09:20 +0530

    implement admin functions

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ const {
   globalErrHandler,
   notFoundErr,
 } = require("../middlewares/globalErrHandler");
+const academicTermRouter = require("../routes/academics/academicTerm");
+const academicYearRouter = require("../routes/academics/academicYear");
 
 const adminRouter = require("../routes/staff/adminRouter");
 
@@ -13,6 +15,8 @@ app.use(express.json()); //pass incoming json data
 
 //Routes
 app.use("/api/v1/admins", adminRouter);
+app.use("/api/v1/academic-years", academicYearRouter);
+app.use("/api/v1/academic-terms", academicTermRouter);
 
 //Error middlewares
 app.use(notFoundErr);
